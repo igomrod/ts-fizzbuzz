264f1f49ad9583427c801e6a1a362b5206ffbdac
Ivan Gomez Rodriguez <ivan.gomezrodriguez@Ivans-MacBook-Pro.local>
2021-10-15 12:01:28 +0200

    first steps

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,1 +1,6 @@
-export const fizzBuzz = () => undefined;
+export const fizzBuzz = (number: number) => {
+    if ( number%3 === 0) {
+        return 'fizz'
+    }
+    return number
+};
--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -1,7 +1,14 @@
 import { fizzBuzz } from '../src/fizzbuzz'
 
 describe('FizzBuzz', () => {
-    it('example test', () => {
-        expect(fizzBuzz()).toEqual(undefined)
+
+    it('should return 1 when input is 1', () => {
+        expect(fizzBuzz(1)).toEqual(1)
     })
+
+    it('should return fizz when input is multiple of 3', () => {
+        expect(fizzBuzz(3)).toEqual('fizz')
+        expect(fizzBuzz(9)).toEqual('fizz')
+    })
+
 })
